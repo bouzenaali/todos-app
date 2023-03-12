d32ed59d646adb286dac4c5163472f8b1be3e5f7
bouzenaali <Alibouzena16@gmail.com>
2023-03-12 23:21:10 +0100

    updated the todoform

--- src/comps/TodoForm.js
+++ src/comps/TodoForm.js
@@ -1,7 +1,14 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect, useRef} from 'react'
 
 function TodoForm(props) {
-    const [input, setInput] = useState('');
+    const [input, setInput] = useState(props.edit ? props.edit.value : '');
+
+    const inputRef = useRef(null);
+
+    useEffect(()=>{
+        inputRef.current.focus();
+    });
+
 
     const handleChange = e =>{
         setInput(e.target.value)
@@ -13,6 +20,7 @@ function TodoForm(props) {
             id: Math.floor(Math.random()*1000),
             text: input
         });
+        setInput('');
     };
 
     
@@ -21,8 +29,35 @@ function TodoForm(props) {
 
   return (
     <form className='todo-form' onSubmit={handleSubmit}>
-        <input type="text" placeholder='add a todo' value={input} name='text' className='todo-input' onChange={handleChange}/>
+
+       {props.edit ? (
+        <>
+        <input 
+            placeholder='update your todo'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            className='todo-input edit'
+            ref={inputRef}
+        />
+
+        <button className='todo-button edit'>update todo</button>
+        </>
+       ) : (
+        <>
+        <input 
+            placeholder='Add a todo'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            className='todo-input'
+            ref={inputRef}
+        />
+
         <button className='todo-button'>add todo</button>
+      
+        </>
+        )}
     </form>
     
   )
--- src/comps/TodoList.js
+++ src/comps/TodoList.js
@@ -1,25 +1,59 @@
 import React, {useState} from 'react'
+import Todo from './Todo';
 import TodoForm from './TodoForm'
+
 
 function TodoList() {
     const [todos, setTodos] = useState([]);
 
     const addTodo = todo =>{
+        if (!todo.text || /^\s*$/.test(todo.text)){
+        return;
+        }
         const newTodos = [todo, ...todos];
-
         setTodos(newTodos);
-        
+        console.log(...todos);
 
     }   
 
+    const updateTodo = (todoId, newValue) =>{
+        if(!newValue.text || /^\s*$/.text(newValue.text)){
+            return;
+        }
+
+        setTodos(prev => prev.map(item => item.id === todoId ? newValue : item));
+    };
+
+    const removeTodo = id =>{
+        const removedArr = [...todos].filter(item => item.id !== id);
+        setTodos(removedArr);
+    }
+
+    const completeTodo = id =>{
+        let updatedTodos = todos.map(todo => {
+            if (todo.id === id){
+                todo.isComplet = !todo.isComplet;
+            };
+            return todo;
+        });
+        setTodos(updatedTodos);
+    }
+
 
   return (
-    
-    <div>
-        <h1>What's the plan for today?</h1>
-        <TodoForm onSubmit={addTodo}/>
-    </div>
+    <>
+        <div>
+            <h1>What's the plan for today?</h1>
+            <TodoForm onSubmit={addTodo}/>
+        </div>
+        <Todo
+        todos = {todos}
+        completeTodo = {completeTodo}
+        removeTodo = {removeTodo}
+        updateTodo = {updateTodo}
+        />
 
+    </>
     )
 }
 
